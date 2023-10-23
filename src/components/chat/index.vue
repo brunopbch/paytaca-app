2ad90ae028375b90baa8610d83f810f9ff395ec5
SeanNikki123 <sslino@up.edu.ph>
2023-10-23 17:19:55 +0800

    updated chat list ui | updated trades ui | remove compose icon on trades menu | added search + menu dialog | small fixes

--- src/components/chat/index.vue
+++ src/components/chat/index.vue
@@ -1,13 +1,5 @@
 <template>
   <div class="q-px-md">
-    <!-- Search -->
-    <!-- <div class="text-center q-pt-lg q-px-lg">
-      <q-input bottom-slots v-model="search" placeholder="Search..." dense rounded outlined :dark="darkMode">
-        <template v-slot:append>
-          <q-icon name="search" />
-        </template>
-      </q-input>
-    </div> -->
     <div class="q-pt-lg">
       <q-card
         class="br-15 q-pt-sm q-mx-none q-mb-lg q-mx-xs"
@@ -17,7 +9,7 @@
         <!-- Top Icons -->
         <div class="text-center row no-wrap items-center q-pa-sm q-pt-md">
           <div class="col-2">
-            <q-icon size="sm" name='sym_o_menu'/>
+            <q-icon size="sm" name='sym_o_menu' @click="menu = true"/>
           </div>
 
           <div class="col-8 text-uppercase bold-text" style="font-size: 18px; font-weight: 500">
@@ -25,7 +17,7 @@
           </div>
 
           <div class="col-2">
-            <q-icon v-show="state !== 'appeals'" size="sm" name='sym_o_create' @click="$emit('openMessage', { info: null, type: 'compose' })" />
+            <q-icon v-show="hasCompose" size="sm" name='sym_o_create' @click="$emit('openMessage', { info: null, type: 'compose' })" />
           </div>
         </div>
 
@@ -42,55 +34,9 @@
             </div>
           </div>
           <div v-else>
-            <!-- CHATS -->
-            <q-pull-to-refresh @refresh="refreshData">
-              <q-list :style="`max-height: ${minHeight - 50}px`" style="overflow:auto;" v-if="state === 'chats' || state === 'trades'">
-                <div v-for="(chat, index) in chats" :key="index">
-                  <q-slide-item @left="onLeft" @right="onRight" right-color="red-6" left-color="grey-5" class=" q-mx-lg">
-                    <q-item clickable @click="$emit('openMessage', { info: chat, type: 'open-message'})" :class="darkMode ? 'pt-dark-card-2 text-white' : ''">
-                      <q-item-section>
-                        <div class="row" >
-                          <div>
-                            <q-avatar size="md" style="padding-top: 13px;">
-                              <q-img style="filter: grayscale(30%);" loading="lazy" spinner-color="white"  :src="`https://ui-avatars.com/api/?background=random&name=${chat.sentFrom.name}&color=fffff`" />
-                            </q-avatar>
-                          </div>
-                          <div  class="q-pt-sm q-mr-md q-pl-xs">
-                            <div class="q-mx-sm" style="font-size: 15px; font-weight: 500;">
-                              {{ chat.sentFrom.name }}
-                            </div>
-                            <div style="font-size: 12px;" class="q-px-sm" :class="darkMode ? 'text-grey-5' : 'text-grey-7'">
-                              <span style="font-size: 13px;">
-                                You: {{ chat.text }}
-                              </span>
-                              &nbsp;<q-icon size=".5em" name='circle'/>&nbsp;
-                              <span>
-                                {{ chat.date }}
-                              </span>
-                            </div>
-
-                          </div>
-                        </div>
-                        <!-- <q-separator :dark="darkMode" class="q-mt-sm"/> -->
-                      </q-item-section>
-                    </q-item>
-
-                    <template v-slot:right>
-                      <span class="text-white">Delete</span>&nbsp;
-                      <q-icon name="delete" />
-                    </template>
-                    <template v-slot:left>
-                      <span class="text-white">Archive</span>&nbsp;
-                      <q-icon name="archive" />
-                    </template>
-                  </q-slide-item>
-                </div>
-              </q-list>
-            </q-pull-to-refresh>
-
             <!-- APPEAL -->
             <q-pull-to-refresh @refresh="refreshData">
-              <q-list v-if="state === 'appeals'" ref="scrollTargetRef" :style="`height: ${minHeight - 100}px`" style="overflow: auto;">
+              <q-list ref="scrollTargetRef" :style="`height: ${minHeight - 100}px`" style="overflow: auto;">
                 <q-infinite-scroll
                   ref="infiniteScroll"
                   :offset="0"
@@ -101,56 +47,126 @@
                       <q-spinner-dots color="primary" size="40px" />
                     </div>
                   </template>
-                  <div v-for="i in 7" :key="i">
-                    <!-- <q-item clickable @click="''" :class="darkMode ? 'pt-dark-card-2 text-white' : ''"> -->
-                      <div class="q-px-lg" @click="$emit('openMessage', { info: chats[0], type: 'open-message'})">
-                        <div class="q-pt-md q-px-md">
-                          <q-badge rounded dense :color="i%2 === 0 ? 'red-6' : 'blue-6'" :label="i%2 === 0 ? 'Release' : 'Refund'" />
-                          <div class="q-pt-xs" style="font-weight: 500;">
-                            Order #001{{ i }}
+
+                  <!-- CHATS -->
+                  <q-list :style="`max-height: ${minHeight - 50}px`" style="overflow:auto;" v-if="state === 'chats'">
+                    <div v-for="(chat, index) in chats" :key="index">
+                      <q-slide-item @left="onLeft" @right="onRight" right-color="red-6" left-color="grey-5" class=" q-mx-lg">
+                        <q-item clickable @click="$emit('openMessage', { info: chat, type: 'open-message'})" :class="darkMode ? 'pt-dark-card-2 text-white' : ''">
+                          <q-item-section>
+                            <div class="row" >
+                              <div>
+                                <q-avatar size="md" style="padding-top: 13px;">
+                                  <q-img style="filter: grayscale(30%);" loading="lazy" spinner-color="white"  :src="`https://ui-avatars.com/api/?background=random&name=${chat.sentFrom.name}&color=fffff`" />
+                                </q-avatar>
+                              </div>
+                              <div  class="q-pt-sm q-mr-md q-pl-xs">
+                                <div class="q-mx-sm" style="font-size: 15px; font-weight: 500;">
+                                  {{ chat.sentFrom.name }}
+                                </div>
+                                <div style="font-size: 12px;" class="q-px-sm" :class="darkMode ? 'text-grey-5' : 'text-grey-7'">
+                                  <span style="font-size: 13px;">
+                                    You: {{ chat.text }}
+                                  </span>
+                                  &nbsp;<q-icon size=".5em" name='circle'/>&nbsp;
+                                  <span>
+                                    {{ chat.date }}
+                                  </span>
+                                </div>
+
+                              </div>
+                            </div>
+                            <!---- <q-separator :dark="darkMode" class="q-mt-sm"/> -->
+                          </q-item-section>
+                        </q-item>
+
+                        <template v-slot:right>
+                          <span class="text-white">Delete</span>&nbsp;
+                          <q-icon name="delete" />
+                        </template>
+                        <template v-slot:left>
+                          <span class="text-white">Archive</span>&nbsp;
+                          <q-icon name="archive" />
+                        </template>
+                      </q-slide-item>
+                    </div>
+                  </q-list>
+
+                  <!-- TRADES -->
+                  <q-list :style="`max-height: ${minHeight - 50}px`" style="overflow:auto;" v-if="state === 'trades'">
+                    <div v-for="i in 7" :key="i">
+                      <!-- <q-item clickable @click="''" :class="darkMode ? 'pt-dark-card-2 text-white' : ''"> -->
+                        <div class="q-px-lg" @click="$emit('openMessage', { info: chats[0], type: 'open-message'})">
+                          <div class="q-pt-md q-px-md">
+                            <!-- <q-badge rounded dense :color="i%2 === 0 ? 'red-6' : 'blue-6'" :label="i%2 === 0 ? 'Release' : 'Refund'" /> -->
+                            <div class="q-pt-xs" style="font-weight: 500;">
+                              Order #001{{ i }}
+                            </div>
+                            <div style="font-size: 12px;" :class="darkMode ? 'text-grey-5' : 'text-grey-7'">
+                              <span style="font-size: 13px;">
+                                Hi there!
+                              </span>
+                              &nbsp;<q-icon size=".5em" name='circle'/>&nbsp;
+                              <span>
+                                {{ i*5 }} min ago
+                              </span>
+                            </div>
+                            <!-- <q-badge outline rounded dense :color="darkMode ? 'blue-grey-3' : 'blue-grey-5'" label="Fiat Ramp" /> -->
                           </div>
-                          <div style="font-size: 12px;" :class="darkMode ? 'text-grey-5' : 'text-grey-7'">
-                            <span style="font-size: 13px;">
-                              Hi there!
-                            </span>
-                            &nbsp;<q-icon size=".5em" name='circle'/>&nbsp;
-                            <span>
-                              {{ i*5 }} min ago
-                            </span>
-                          </div>
-                          <q-badge outline rounded dense :color="darkMode ? 'blue-grey-3' : 'blue-grey-5'" label="Fiat Ramp" />
+                          <q-separator :dark="darkMode" class="q-mt-sm q-mx-sm"/>
                         </div>
-                        <q-separator :dark="darkMode" class="q-mt-sm q-mx-sm"/>
-                      </div>
-                    <!-- </q-item> -->
-                  </div>
+                      <!-- </q-item> -->
+                    </div>
+                  </q-list>
+
+                  <!-- APPEALS -->
+                  <q-list :style="`max-height: ${minHeight - 50}px`" style="overflow:auto;" v-if="state === 'appeals'">
+                    <div v-for="i in 7" :key="i">
+                      <!-- <q-item clickable @click="''" :class="darkMode ? 'pt-dark-card-2 text-white' : ''"> -->
+                        <div class="q-px-lg" @click="$emit('openMessage', { info: chats[0], type: 'open-message'})">
+                          <div class="q-pt-md q-px-md">
+                            <q-badge rounded dense :color="i%2 === 0 ? 'red-6' : 'blue-6'" :label="i%2 === 0 ? 'Release' : 'Refund'" />
+                            <div class="q-pt-xs" style="font-weight: 500;">
+                              Order #001{{ i }}
+                            </div>
+                            <div style="font-size: 12px;" :class="darkMode ? 'text-grey-5' : 'text-grey-7'">
+                              <span style="font-size: 13px;">
+                                Hi there!
+                              </span>
+                              &nbsp;<q-icon size=".5em" name='circle'/>&nbsp;
+                              <span>
+                                {{ i*5 }} min ago
+                              </span>
+                            </div>
+                            <q-badge outline rounded dense :color="darkMode ? 'blue-grey-3' : 'blue-grey-5'" label="Fiat Ramp" />
+                          </div>
+                          <q-separator :dark="darkMode" class="q-mt-sm q-mx-sm"/>
+                        </div>
+                      <!-- </q-item> -->
+                    </div>
+                  </q-list>
                 </q-infinite-scroll>
               </q-list>
             </q-pull-to-refresh>
-
-            <!-- <q-list> -->
-              <!-- <div  class="q-pt-md q-mx-lg q-px-md">
-                <div class="q-mx-sm" style="font-size: 15px; font-weight: 500;">
-                  {{ chat.sentFrom.name }}
-                </div>
-                <div style="font-size: 12px;" class="text-grey-7 q-px-sm">
-                  <span style="font-size: 13px;">
-                    {{ chat.text }}
-                  </span>
-                  &nbsp;<q-icon size=".5em" name='circle'/>&nbsp;
-                  <span>
-                    {{ chat.date }}
-                  </span>
-                </div>
-
-                <q-separator :dark="darkMode" class="q-mt-sm"/>
-              </div> -->
-            <!-- </q-list> -->
           </div>
         </div>
       </q-card>
     </div>
   </div>
+  <q-dialog v-model="menu" persistent>
+    <q-card style="width: 100%;" class="br-15">
+        <div class="row q-py-md q-px-sm">
+          <q-icon size="sm" name="close" v-close-popup/>
+        </div>
+        <div class="text-center q-px-lg">
+          <q-input bottom-slots v-model="search" placeholder="Search..." dense rounded outlined :dark="darkMode">
+            <template v-slot:append>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+        </div>
+    </q-card>
+  </q-dialog>
 </template>
 <script>
 import { ref } from 'vue'
@@ -163,7 +179,7 @@ export default {
     }
   },
   computed: {
-    disableLeftSlider () {
+    hasCompose () {
       return this.state === 'chats'
     }
   },
@@ -172,6 +188,7 @@ export default {
       darkMode: this.$store.getters['darkmode/getStatus'],
       minHeight: this.$q.platform.is.ios ? this.$q.screen.height - (95 + 130) : this.$q.screen.height - (70 + 110),
       search: '',
+      menu: false,
       chats: [
         {
           id: 1,
@@ -211,6 +228,7 @@ export default {
   emits: ['openMessage'],
   async mounted () {
     console.log('chat body')
+    this.isloaded = true
   },
   methods: {
     refreshData (done) {
